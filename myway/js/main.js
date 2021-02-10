201f5813d079973b4d15de3f2de85c81eca6c301
goodelias <55098894+goodelias@users.noreply.github.com>
2021-02-10 16:42:58 +0200

    wayup

--- myway/js/main.js
+++ myway/js/main.js
@@ -9,4 +9,14 @@ $(function () {
             }
         });
     });
+    $(window).scroll(function() {
+        $('#mail .section-title').each(function(){
+            var imagePos = $(this).offset().top;
+
+            var topOfWindow = $(window).scrollTop();
+            if (imagePos < topOfWindow+650) {
+                $(this).addClass("fadeInLeft");
+            }
+        });
+    });
 })
