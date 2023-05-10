3359429900feca61c3a93580ba66e920f1bf3eb8
Ilia Cepchin <ilia@ilias-mbp.home>
2023-05-10 21:32:51 +0100

    Add mern-blog-frontend project

---
+++ mern-blog-frontend/src/axios.js
@@ -0,0 +1,18 @@
+import axios from "axios";
+
+const instance = axios.create({
+  baseURL: "http://localhost:4444",
+});
+
+instance.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem("token");
+    if (token) {
+      config.headers.Authorization = token;
+    }
+    return config;
+  },
+  (error) => Promise.reject(error)
+);
+
+export default instance;
---
+++ mern-blog-frontend/src/pages/index.js
@@ -0,0 +1,5 @@
+export { Home } from "./Home";
+export { FullPost } from "./FullPost";
+export { AddPost } from "./AddPost";
+export { Signup } from "./Signup";
+export { Login } from "./Login";
